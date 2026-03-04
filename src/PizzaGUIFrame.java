e05349ee56ca50f902fada8cee27d1af08a583a6
diepveetee <diepvt@mail.uc.edu>
2026-03-04 17:11:40 -0500

    Added frame and configured it to be displayed when ran.

--- src/PizzaGUIFrame.java
+++ src/PizzaGUIFrame.java
@@ -1,2 +1,36 @@
-public class PizzaGUIFrame {
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PizzaGUIFrame extends JFrame {
+
+    public PizzaGUIFrame(){
+        super("Pizza GUI");
+
+        setLayout((new BorderLayout()));
+
+        configureFrame();
+
+    }
+
+
+    private void configureFrame() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = toolkit.getScreenSize();
+
+        int width = (int)(screenSize.width * 0.75);
+        int height = (int)(screenSize.height * 0.75);
+
+        setSize(width, height);
+        setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+
+
 }
